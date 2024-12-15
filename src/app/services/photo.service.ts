22529f2647fbf3985088b4a2fe0cb9757fbba61f
CDUGUINE <cduguine@gmail.com>
2024-12-15 09:56:06 +0100

    3. Loading photos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -9,64 +9,76 @@ import { Preferences } from '@capacitor/preferences';
 })
 export class PhotoService {
   public photos: UserPhoto[] = [];
-
-  // other code
-
+  private PHOTO_STORAGE: string = 'photos';
+  
   constructor() { }
 
-  private async savePicture(photo: Photo) { 
-    // Convert photo to base64 format, required by Filesystem API to save
-    const base64Data = await this.readAsBase64(photo);
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photoList = await Preferences.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photoList.value!) || [];
 
-    // Write the file to the data directory
-    const fileName = Date.now() + 'jpg';
-    const savedFile = await Filesystem.writeFile({
-      path: fileName,
-      data: base64Data,
-      directory: Directory.Data
-    });
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data
+      });
 
-    // Use webPath to display the new image instead of base64 since it's
-    // already loaded into memory
-    return {
-      filepath: fileName,
-      webviewPath: photo.webPath
-    };
-   }
+      // Web plateform only: Load the photo as base64 data
+      photo.webviewPath = 'data:image/jpeg;base64,$(readFile.data)';
+    }
+  }
+
+  private async savePicture(photo: Photo) {
+  // Convert photo to base64 format, required by Filesystem API to save
+  const base64Data = await this.readAsBase64(photo);
+
+  // Write the file to the data directory
+  const fileName = Date.now() + '.jpeg';
+  const savedFile = await Filesystem.writeFile({
+    path: fileName,
+    data: base64Data,
+    directory: Directory.Data
+  });
+
+  // Use webPath to display the new image instead of base64 since it's
+  // already loaded into memory
+  return {
+    filepath: fileName,
+    webviewPath: photo.webPath
+  };
+}
 
    private async readAsBase64(photo: Photo) {
-    // Fetch the photo, read as a lob, then convert to base64 format
+    // Fetch the photo, read as a blob, then convert to base64 format
     const response = await fetch(photo.webPath!);
     const blob = await response.blob();
-
+  
     return await this.convertBlobToBase64(blob) as string;
-   }
-
-   private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+  }
+  
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.onerror = reject;
     reader.onload = () => {
-      resolve(reader.result);
+        resolve(reader.result);
     };
     reader.readAsDataURL(blob);
-   });
+  });
 
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri, // file-based data; provides best performance
       source: CameraSource.Camera, // automatically take a new photo with the camera
-      quality: 100 // higest quality (0 to 100)
+      quality: 100 // highest quality (0 to 100)
     });
-
-    // save the picture and add it to photo collection
+  
+    // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
-
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath!
-    });
   }
 }
 
